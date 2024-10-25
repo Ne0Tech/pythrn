1c872aef9fcab2f6396e2125da7f1e06452c7859
Zavian Pedigo <pedigzav001@warren.k12.in.us>
2024-10-25 09:59:42 -0400

    op

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-age = 20
+age = int(input("Enter your age: "))
 
-if age 
+if age < 5:
+    print("You should play with toys.")
+elif 5 <= age <= 12:
+    print("You can play outdoor games.")
+elif 13 <= age <= 17:
+    print("You can play video games.")
+else:
+    age >= 18
+    print("You could try hiking. ")
